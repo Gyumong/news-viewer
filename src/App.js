4c63384d7217b584aa0e16b166d2d9bdcf421639
Gyumong <rlaalsrb3559@naver.com>
2020-03-27 18:01:56 +0900

    UI 만들고 NEW API 적용하기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App = ()=>{
-  const [data,setData] =useState(null);
-  const onClick=async ()=>{
-    try{
-      const response = await axios.get(
-    'http://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=f4266e8ad53f47aea9baad9a404e8859'
-    );
-      setData(response.data);
-    } catch(e){
-      console.log(e);
-    }
-  };
-  
-  return(
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
-    </div>
-  );
+  return<NewsList />
 };
 
 export default App;
